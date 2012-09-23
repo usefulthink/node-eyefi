60c824f1c4b02b10b1ff2206ce2bb5cc87d56ec4
Martin Schuhfuss <m.schuhfuss@googlemail.com>
2012-09-23 02:35:10 +0200

    cleaned up, now also uses the soapParser-middleware

--- routes/upload.js
+++ routes/upload.js
@@ -1,61 +1,30 @@
-var qs = require('querystring'),
-    xml2js = require('xml2js'),
-    config = require('../lib/config'),
+var config = require('../lib/config'),
     tar = require('tar'),
     fs = require('fs'),
-    util = require('util'),
-    path = require('path'),
-    url = require('url');
+    path = require('path');
 
-var parser = new xml2js.Parser(xml2js.defaults["0.1"]);
-
-
-/*
-processes an upload-requests:
-
-POST /api/soap/eyefilm/v1/upload
-req.headers = {
-  host: 'api.eye.fi',
-  user-agent: 'Eye-Fi Card/5.0009',
-  content-type: 'multipart/form-data; ...',
-  content-length: nBytes
-},
-req.files.FILENAME: attached file is a tar-file containing a single file (?)
-
-req.body.INTEGRITYDIGEST: i'd assume some kind of verification for the SOAP-Request, but don't know what kind of digest.
-req.body.SOAPENVELOPE: the soap-message
-*/
-
+// TODO: we need to emit an event when the file upload is complete.
+//       Stick to the express-middleware conventions and make the router configurable.
+// TODO: logging with console.log needs to be replaced
 
 module.exports = function (req, res) {
-    function renderUpload(err, data) {
-        var obj = data["SOAP-ENV:Body"]["ns1:UploadPhoto"],
-            cardConfig = config.getCardConfig(obj.macaddress);
+    var obj = req.soap.data["SOAP-ENV:Body"]["ns1:UploadPhoto"],
+        uploadPath = config.getUploadPath(obj.macaddress);
 
-        var uploadPath = config.getUploadPath(obj.macaddress);
-        var uploadedFile = req.files.FILENAME;
+    fs.createReadStream(req.files.FILENAME.path)
+        .pipe(tar.Parse())
+        .on("error", function (err) {
+            console.error("an error occurred while extracting uploaded data: " + err);
+            res.end();
+        })
+        .on("entry", function (entry) {
+            console.log('tar entry: ' + entry.props.path);
 
-        console.log(JSON.stringify(uploadedFile));
-
-        fs.createReadStream(req.files.FILENAME.path)
-            .pipe(tar.Parse())
-            .on("error", function (err) {
-                console.error("an error occurred while extracting uploaded data: " + err);
-                res.end();
-            })
-            .on("entry", function (entry) {
-                console.log('tar entry: ' + entry.props.path);
-
-                var outfile = path.join(uploadPath, entry.props.path);
-                entry.pipe(fs.createWriteStream(outfile));
-
-                entry.on("end", function () {
-                    console.log("entry ended")
-                });
-            })
-            .on("end", function () { res.render('uploadSuccess'); });
-    };
-
-    var decodedBody = decodeURIComponent(qs.stringify(qs.parse(req.body.SOAPENVELOPE)));
-    parser.parseString(decodedBody, renderUpload);
+            var outfile = path.join(uploadPath, entry.props.path);
+            entry.pipe(fs.createWriteStream(outfile));
+            entry.on("end", function () {
+                console.log('upload complete. file: ', outfile);
+            });
+        })
+        .on("end", function () { res.render('uploadSuccess'); });
 };
