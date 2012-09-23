39820543d2144f1216f6375093fc38d2702a83cf
Martin Schuhfuss <m.schuhfuss@googlemail.com>
2012-09-23 17:16:29 +0200

    prepare to replace logging

--- app.js
+++ app.js
@@ -3,13 +3,17 @@
 var fs = require('fs'),
     path = require('path'),
     eyefi = require('./lib/server'),
+    Logger = require('devnull'),
 
     config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json')));
+
+
+config.logger = new Logger();
 
 var eyefiServer = eyefi.createServer(config).start();
 
 console.log("node-eyefi started and listening");
 
 eyefiServer.on('imageReceived', function(imgData) {
-    console.log('received an image: ' + imgData.filename);
+    config.logger.info('received an image: ' + imgData.filename);
 });
