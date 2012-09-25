29c4a113d5e71a356c159a068b519898a334d7aa
Martin Schuhfuss <m.schuhfuss@googlemail.com>
2012-09-25 20:38:33 +0200

    upload-progress and streaming uploads

--- standalone.js
+++ standalone.js
@@ -3,18 +3,33 @@
 var fs = require('fs'),
     path = require('path'),
     eyefi = require('./lib/server'),
+    bytes = require('bytes'),
     Logger = require('devnull'),
 
     config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json')));
 
-config.logger = new Logger({
-    level: 6
+var logger = config.logger = new Logger({
+    level: 9
 });
 
 var eyefiServer = eyefi(config).start();
 
-config.logger.notice("eyefi-server started and listening");
+logger.notice("eyefi-server started and listening");
 
-eyefiServer.on('imageReceived', function(imgData) {
-    config.logger.notice('received an image: ' + imgData.filename);
+eyefiServer.on('imageReceived', function __logImageReceived(imgData) {
+    logger.notice('received an image: ' + imgData.filename);
+});
+
+var progressLoggingPaused = false;
+eyefiServer.on('uploadProgress', function(progressData) {
+    var br=progressData.received || 0,
+        be=progressData.expected || 0,
+        pct=(100*br/be).toFixed(2);
+
+    if(!progressLoggingPaused  || br==be) {
+        logger.metric('progress: ' + bytes(br) + ' of ' + bytes(be) + '(' + pct + '%)');
+
+        progressLoggingPaused = true;
+        setTimeout(function() { progressLoggingPaused=false; }, 100);
+    }
 });
